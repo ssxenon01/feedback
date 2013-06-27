1a631247a02b86994e4ec443c8e8a0a8e942b887
Gundsambuu <ssxenon01@gmail.com>
2013-06-27 10:20:04 +0800

    Updates.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -45,10 +45,10 @@ grails.project.dependency.resolution = {
 
     plugins {
         runtime ":hibernate:$grailsVersion"
-        // runtime ":jquery:1.8.3"
-        // compile ":jquery-ui:1.8.24"
-        // compile ":famfamfam:1.0.1"
-        // runtime ":resources:1.1.6"
+        runtime ":jquery:1.8.3"
+        compile ":jquery-ui:1.8.24"
+//        compile ":famfamfam:1.0.1"
+//        runtime ":resources:1.1.6"
         compile ":webxml:1.4.1"
         
         // Uncomment these (or add new ones) to enable additional resources capabilities
